43a87622a13affd71f62c9aaa0393db7b554ae5e
ETCasual <casualorient@gmail.com>
2024-04-28 22:00:53 +0800

    update splidejs types

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -1,11 +1,3 @@
-export {};
-
-declare global {
-  interface Window {
-    workbox: string;
-  }
-}
-
 declare module "@splidejs/react-splide" {
   export { Options } from "@splidejs/splide";
   export { Splide, SplideSlide } from "@splidejs/react-splide";
